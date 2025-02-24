eefe9d532e5d624140baa2b0ae5e48f11fa48050
Alessandro Sylar <lsn.lodigiani@gmail.com>
2025-02-24 13:05:34 +0100

    ADD: aggiunta configurazione Prettier e miglioramenti di formattazione nel componente App

--- 47-react-form/src/App.jsx
+++ 47-react-form/src/App.jsx
@@ -1,89 +1,77 @@
-import { useState } from "react"
+import { useState } from 'react';
 
 const initialTasks = ['Fare la spesa', 'Pulire casa', 'Fare il bucato'];
 
 function App() {
-
-  const [firstName, setFirstName] = useState('Mario')
-
+  const [firstName, setFirstName] = useState('Mario');
 
   //todolist
   //l'array iniziale reattivo
-  const [tasks, setTasks] = useState( initialTasks ) 
+  const [tasks, setTasks] = useState(initialTasks);
   //Nuovo task da aggiungere all'array iniziale
-  const [newTask, setNewTask] = useState('')
+  const [newTask, setNewTask] = useState('');
 
   const handleChange = (e) => {
-    console.log( e )
-    console.log( e.target )
-    setFirstName( e.target.value )
-  }
+    console.log(e);
+    console.log(e.target);
+    setFirstName(e.target.value);
+  };
 
   const addTask = (e) => {
-    e.preventDefault()
-    const task = newTask.trim()
-    setTasks( [...tasks, task] )
-    setNewTask('')
-   }
+    e.preventDefault();
+    const task = newTask.trim();
+    setTasks([...tasks, task]);
+    setNewTask('');
+  };
 
   const removeTask = (indiceElementoArray) => {
-    console.log( indiceElementoArray )
-    const arrayClone = tasks.filter( ( _, index ) => index !== indiceElementoArray )
-    return setTasks( arrayClone )
-
-  }
+    console.log(indiceElementoArray);
+    const arrayClone = tasks.filter(
+      (_, index) => index !== indiceElementoArray
+    );
+    return setTasks(arrayClone);
+  };
 
   return (
     <>
-      <input
-        type="text"
-        value={firstName}
-        onChange={ handleChange  }  
-      />
+      <input type="text" value={firstName} onChange={handleChange} />
 
-      <p> Il tuo nome è: { firstName } </p>
-
+      <p> Il tuo nome è: {firstName} </p>
 
       <div className="container mt-5 w-50">
         <h1>tasks</h1>
         <ul className="list-group">
-          {
-            tasks.map( ( element, index ) => {
-              return(
-                <li key={index} className="list-group-item">
-                  { element }
+          {tasks.map((element, index) => {
+            return (
+              <li key={index} className="list-group-item">
+                {element}
 
-                <button className="btn btn-danger btn-sm float-end" onClick={ () => removeTask(index) }>
+                <button
+                  className="btn btn-danger btn-sm float-end"
+                  onClick={() => removeTask(index)}
+                >
                   X
                 </button>
-
-                </li>
-              )
-            } )
-          }
+              </li>
+            );
+          })}
         </ul>
 
-
-
         {/* //form per il nuovo items  */}
-        <form onSubmit={ addTask }>
-          <input 
+        <form onSubmit={addTask}>
+          <input
             type="text"
             className="form-control"
             placeholder="Cosa devi fare?"
-            value={ newTask }
-            onChange={ (e) => setNewTask( e.target.value ) }
-            />
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+          />
 
           <button className="btn btn-primary mt-3">Aggiungi</button>
-
         </form>
-
-
       </div>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
