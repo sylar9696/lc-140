5cb89cfcce06ebdb6d681f2a70c4cd4c047c0992
Alessandro Sylar <lsn.lodigiani@gmail.com>
2025-02-27 12:43:55 +0100

    ADD: aggiunta file .env-example per la configurazione dell'API e aggiornato .gitignore per escludere file .env; integrazione di Bootstrap in index.html e implementazione di funzionalità di gestione delle pizze in App.jsx

--- 49-react-fetch/src/App.jsx
+++ 49-react-fetch/src/App.jsx
@@ -4,11 +4,19 @@ import axios from 'axios';
 export default function App() {
   //creare un array vuoto al cui interno andranno i dati della chiamata fetch
   //array reattivo
-  const [todos, setTodos] = useState([]);
+  // const [todos, setTodos] = useState([]);
+
+  const apiUrl = import.meta.env.VITE_BASE_API_URL;
 
   const [pizzas, setPizzas] = useState([]);
 
+  const initialFormData = {
+    name: '',
+    image: '',
+    ingredients: [],
+  };
 
+  const [formData, setFormData] = useState(initialFormData);
 
   // Fetching dei dati
   // function fetchTodos() {
@@ -19,25 +27,74 @@ export default function App() {
   //   .then(setTodos);
   // }
 
-  function fetchTodos() {
-    axios
-      .get('https://jsonplaceholder.typicode.com/todos')
-      .then((res) => setTodos(res.data));
-  }
+  // function fetchTodos() {
+  //   axios
+  //     .get('https://jsonplaceholder.typicode.com/todos')
+  //     .then((res) => setTodos(res.data));
+  // }
 
   function fetchPizzas() {
-    axios
-      .get('http://localhost:3000/api/pizzas')
-      .then((res) => setPizzas(res.data));
+    axios.get(apiUrl).then((res) => setPizzas(res.data));
   }
 
+  const handleDelete = (idSingolaPizza) => {
 
+    fetch( `${apiUrl}/${idSingolaPizza}`, {
+      method: 'DELETE'
+    } ).then(
+      fetchPizzas()
+    )
+
+    // axios.delete(`${apiUrl}/${idSingolaPizza}`).then(
+    //   fetchPizzas()
+    //   //setPizzas(pizzas.filter( elem => elem.id !== idSingolaPizza ))
+    // );
+  };
+
+  const handleField = (event) => {
+    //e.target si occupa di recuperare in modo dinamico il campo che stiamo compilando del form
+    const { name, value } = event.target;
+    console.log(event.target); //<input type="text" name="name">
+    console.log(event);
+    console.log('attributo del input: ' + name);
+    console.log('value del input: ' + value);
+
+    //condizione per tramutare la stringa in array
+    if ([name] == 'ingredients') {
+      setFormData({
+        //...formData è un operatore di spread che mi permette di copiare l'oggetto formData
+        ...formData,
+        [name]: value.split(',').map((elem) => elem.trim()),
+      });
+    } else {
+      setFormData({
+        //...formData è un operatore di spread che mi permette di copiare l'oggetto formData
+        ...formData,
+        [name]: value,
+      });
+    }
+
+    console.log(formData);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    axios
+      .post(apiUrl, formData)
+      .then(
+        fetchPizzas()
+        //svuotiamo l'array formData
+      )
+      .catch((err) => console.log(err));
+
+    setFormData(initialFormData);
+  };
 
   // Solo al primo rendering
-  useEffect(fetchTodos, []);
+  // useEffect(fetchTodos, []);
 
   useEffect(fetchPizzas, []);
-
 
   return (
     <div>
@@ -45,20 +102,76 @@ export default function App() {
       {/* <button onClick={fetchTodos}>Carica Todos</button> */}
       <ul>
         {/* ciclo che funziona nel momento in cui l'array vuoto si popola con la chiamata fetch */}
-        {todos.map((todo) => (
+        {/* {todos.map((todo) => (
           <li key={todo.id}>{todo.title}</li>
-        ))}
+        ))} */}
       </ul>
 
-        {/* pizzas */}
-      <ul>
-        {/* ciclo che funziona nel momento in cui l'array vuoto si popola con la chiamata fetch */}
-        {pizzas.map((elem) => (
-          <li key={elem.id}>{elem.name}</li>
-        ))}
-      </ul>
+      {/* pizzas */}
+      <div className="container">
+        <ul className="d-flex flex-column gap-3">
+          {/* ciclo che funziona nel momento in cui l'array vuoto si popola con la chiamata fetch */}
+          {pizzas.map((elem) => (
+            <li key={elem.id} className="d-flex justify-content-between">
+              <h4>{elem.name}</h4>
+              <ul>
+                {elem.ingredients.map((ingredient, index) => (
+                  <li key={index}>{ingredient}</li>
+                ))}
+              </ul>
 
-      
+              <button
+                className="btn btn-danger"
+                onClick={() => handleDelete(elem.id)}
+              >
+                x
+              </button>
+            </li>
+          ))}
+        </ul>
+
+        {/* form */}
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="">Name</label>
+            <input
+              type="text"
+              name="name"
+              onChange={handleField}
+              value={formData.name}
+            />
+
+            <label htmlFor="">image</label>
+            <input
+              type="text"
+              name="image"
+              onChange={handleField}
+              value={formData.image}
+            />
+
+            <label htmlFor="">ingredients</label>
+            <input
+              type="text"
+              name="ingredients"
+              onChange={handleField}
+              value={formData.ingredients}
+            />
+
+            <button className="btn btn-primary" type="submit">
+              Invio
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
+
+/*
+
+chiamta api delete
+evento sul bottone: onClick
+
+funzione di chiamata api all'onCLick
+
+*/
--- 50-react-fetch-rick/src/App.jsx
+++ 50-react-fetch-rick/src/App.jsx
@@ -4,9 +4,15 @@ function App() {
   //1-impostiamo array vuoto per i personaggi dell'api
   const [characters, setCharacters] = useState([]);
 
+  const [page, setPage] = useState(1);
+
+  const endpoint = 'https://rickandmortyapi.com/api/character?page='
+
   //2- impostiamo la chiamata al caricamento del componente
-  const fetchCharacters = () => {
-    fetch('https://rickandmortyapi.com/api/character')
+  const fetchCharacters = ( currentPage ) => {
+
+    //https://rickandmortyapi.com/api/character?page=1
+    fetch(endpoint + currentPage)
       .then((res) => res.json())
       .then((data) => setCharacters(data.results))
       // .then(
@@ -23,11 +29,26 @@ function App() {
 
   //attiviamo la chiamata api senza dipendenze [], cosi da occuparci solo dei cicli di vita del compoennte
   //eseguito al caricamento del componente
-  useEffect(fetchCharacters, []);
+  useEffect( () => fetchCharacters( page ), [ page ]);
+
+  const goPrev = () => {
+    setPage( page - 1 );
+  }
+
+  const goNext = () => {
+    setPage( page + 1 );
+  }
 
   return (
     <>
       <div className="container">
+
+        {/* paginazione */}
+        <div>
+          <button onClick={goPrev} disabled={ ( page === 1 ) ? true : false } >Prev</button>
+          <button onClick={goNext} disabled={ ( page === 42 ) ? true : false } >Next</button>
+        </div>
+
         <div className="row row-cols-4">
           {characters.map((elem) => {
             return (
