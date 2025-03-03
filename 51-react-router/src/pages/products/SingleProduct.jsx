1d09271647c8bda316bf48fbdfc96946d5d12aa5
Alessandro Sylar <lsn.lodigiani@gmail.com>
2025-03-03 11:43:36 +0100

    ADD: aggiunta configurazione per l'endpoint API con file .env-example; aggiornato .gitignore per escludere file .env; implementazione della gestione dei prodotti e del singolo prodotto con fetch in Products.jsx e SingleProduct.jsx

--- 51-react-router/src/pages/products/SingleProduct.jsx
+++ 51-react-router/src/pages/products/SingleProduct.jsx
@@ -1,12 +1,59 @@
 //import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+// import axios from "axios";
 
 const SingleProduct = () => {
-
     const { id } = useParams()
 
+    const url = import.meta.env.VITE_ENDPOINT_URL
+
+    const navigate = useNavigate()
+
+
+    const [ singlePizza, setSinglePizza ] = useState( {
+        id: '',
+        name: '',
+        image: '',
+        ingredients: []
+    } )
+
+
+    
+
+    useEffect( () => {
+        // axios.get( `${url}/${id}` )
+        //     .then( res => setSinglePizza( res.data ) )
+
+        fetch( `${url}/${id}`, { method: 'GET' } )
+            .then( res => res.json() )
+            .then( data => setSinglePizza( data ) )
+
+            // navigate('/pizzas')
+
+    }, [id] ) 
+
+
     return(
-        <h1>Singolo prodotto: { id }</h1>
+        <>
+            <h1>Singolo prodotto: { id }</h1>
+            <h2>Titolo: { singlePizza.name }</h2>
+            
+            <ul>
+
+                {
+                    singlePizza.ingredients.map( (elem, id) => {
+                        return(
+                            <li key={id}> {elem} </li>
+                        )
+                    }  )
+                }
+            </ul>
+
+            <button onClick={() => navigate(-1)}>
+                torna alla pagina precedente
+            </button>
+        </>
     )
 }
 
